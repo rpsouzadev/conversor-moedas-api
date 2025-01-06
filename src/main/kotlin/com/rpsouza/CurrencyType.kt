33fbd886360b6df9a9af18e9d7e9d3353749d041
Rafael Souza <rpsouza.dev@gmail.com>
2025-01-06 12:46:17 -0300

    Add currency_type route

---
+++ src/main/kotlin/com/rpsouza/CurrencyType.kt
@@ -0,0 +1,79 @@
+package com.rpsouza
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CurrencyType(
+    val name: String,
+    val acronym: String,
+    val symbol: String,
+    val countryFlagImage: String,
+)
+
+@Serializable
+data class CurrencyTypesResult(
+    val values: List<CurrencyType>
+)
+
+val currencyList = listOf(
+    CurrencyType(
+        name = "Real Brasileiro",
+        acronym = "BRL",
+        symbol = "R$",
+        countryFlagImage = "https://flagcdn.com/br.svg"
+    ),
+    CurrencyType(
+        name = "Dólar Americano",
+        acronym = "USD",
+        symbol = "$",
+        countryFlagImage = "https://flagcdn.com/us.svg"
+    ),
+    CurrencyType(
+        name = "Euro",
+        acronym = "EUR",
+        symbol = "€",
+        countryFlagImage = "https://flagcdn.com/eu.svg"
+    ),
+    CurrencyType(
+        name = "Iene Japonês",
+        acronym = "JPY",
+        symbol = "¥",
+        countryFlagImage = "https://flagcdn.com/jp.svg"
+    ),
+    CurrencyType(
+        name = "Libra Esterlina",
+        acronym = "GBP",
+        symbol = "£",
+        countryFlagImage = "https://flagcdn.com/gb.svg"
+    ),
+    CurrencyType(
+        name = "Dólar Australiano",
+        acronym = "AUD",
+        symbol = "A$",
+        countryFlagImage = "https://flagcdn.com/au.svg"
+    ),
+    CurrencyType(
+        name = "Dólar Canadense",
+        acronym = "CAD",
+        symbol = "C$",
+        countryFlagImage = "https://flagcdn.com/ca.svg"
+    ),
+    CurrencyType(
+        name = "Franco Suíço",
+        acronym = "CHF",
+        symbol = "CHF",
+        countryFlagImage = "https://flagcdn.com/ch.svg"
+    ),
+    CurrencyType(
+        name = "Yuan Chinês",
+        acronym = "CNY",
+        symbol = "¥",
+        countryFlagImage = "https://flagcdn.com/cn.svg"
+    ),
+    CurrencyType(
+        name = "Rúpia Indiana",
+        acronym = "INR",
+        symbol = "₹",
+        countryFlagImage = "https://flagcdn.com/in.svg"
+    )
+)
--- src/main/kotlin/com/rpsouza/Routing.kt
+++ src/main/kotlin/com/rpsouza/Routing.kt
@@ -1,15 +1,28 @@
 package com.rpsouza
 
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/currency_types") {
+            call.respond(
+                CurrencyTypesResult(values = currencyList)
+            )
+        }
+
+        get("/exchange_rate/{from}/{to}") {
+            val from = call.parameters["from"]?.uppercase() ?: return@get call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "Não foi possível obter o acrônimo da moeda atual."
+            )
+
+            val to = call.parameters["to"]?.uppercase() ?: return@get call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "Não foi possível obter o acrônimo da moeda alvo."
+            )
         }
     }
 }
